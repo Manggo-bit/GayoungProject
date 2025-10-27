e61cf8c03cfc6c7f093ca625b30d80c13a6830a2
young211 <love90230500@gmail.com>
2025-10-28 00:20:01 +0900

    새로운 기능 :

--- DragonsDilemma/vite.config.js
+++ DragonsDilemma/vite.config.js
@@ -1,11 +1,8 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base 경로를 "/GayoungProject/DragonsDilemma/"로 수정합니다.
-  base: "/GayoungProject/",
+  base: "/DragonsDilemma/",
   plugins: [react()],
 })
